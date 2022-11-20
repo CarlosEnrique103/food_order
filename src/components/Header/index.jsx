6859771a29d3cd16359b64866160161ad0269b61
Juan Carlos Echevarria <juancarlosenrique103@gmail.com>
2022-11-20 07:33:49 -0500

    [feat]: Add icon for the title and header :zap:.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,12 +2,16 @@ import { Fragment } from "react";
 import foodBackground from "./../../assets/header.jpg";
 import HeaderButton from "./HeaderButton";
 import classes from "./classes.module.css";
-
+import Meal from "./../../assets/icon/food.svg";
 const Header = () => {
 	return (
 		<Fragment>
 			<div className={classes.header}>
-				<h1>ReactJS Food</h1>
+				<div className={classes["header-icon"]}>
+					<img src={Meal} alt='Icon meal' />
+					<h1>ReactJS Food</h1>
+				</div>
+
 				<HeaderButton />
 			</div>
 
